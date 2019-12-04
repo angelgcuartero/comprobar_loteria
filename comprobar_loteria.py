ca382d5c68fa993c786627dded58082ed64b7795
Angel García <angel.garcia@devo.com>
2019-12-04 12:14:03 +0100

    Added file passed by argument and error checks

--- comprobar_loteria.py
+++ comprobar_loteria.py
@@ -3,20 +3,40 @@
 
 import requests
 import json
+import sys
+
 
 api_url = 'http://api.elpais.com/ws/LoteriaNavidadPremiados'
 # http://api.elpais.com/ws/LoteriaNavidadPremiados?n=33488
 # http://api.elpais.com/ws/LoteriaNavidadPremiados?s=1  --> pide status del sorteo
 
 def main():
-    number_list = [3347, 44212, 48484, 9902, 1]
+    """Recibe una lista de números en un fichero, uno en cada línea, y devuelve el premio"""
+    
+    if len(sys.argv) != 2:
+        print('Por favor, introduzca el nombre de fichero con la lista de números.')
+        return -1
 
-    for n in number_list:
-        response = requests.get('{}?n={}'.format(api_url, n))
+    input_file = sys.argv[1]
+
+    try:
+        with open(input_file) as in_f:
+            lines = in_f.readlines()
+    except FileNotFoundError:
+        print(f'No se encuentra el fichero {input_file}')
+        return -2
+        
+    lines = [x.strip() for x in lines] 
+
+    for n in lines:
+        response = requests.get(f'{api_url}?n={n}')
         json_data = json.loads(response.text[9:])
-        print('El número {:5} tiene un premio de: {:8}'.format(n, int(json_data['premio'])))
+        result = 'error' if json_data['error'] else int(json_data['premio'])
+        print(f'El número {int(n):05} tiene un premio de: {result}')
+    
+    return 0
 
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
     
