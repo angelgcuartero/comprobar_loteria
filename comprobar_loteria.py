b59b565cfbf5e5df83ea21b51c421058f1b6806d
Ángel García Cuartero <anhelido@gmail.com>
2019-12-09 12:13:36 +0100

    Added status message before checking numbers

--- comprobar_loteria.py
+++ comprobar_loteria.py
@@ -28,9 +28,23 @@ def main():
         
     lines = [int(x.strip()) for x in lines] 
 
+    # Get lottery status
+    status_msg = [
+        "El sorteo no ha comenzado aún. Todos los números aparecerán como no premiados.",
+        "El sorteo ha empezado. La lista de números premiados se va cargando poco a poco. \nUn número premiado podría llegar a tardar unos minutos en aparecer.",
+        "El sorteo ha terminado y la lista de números y premios debería ser la correcta aunque, tomada al oído, no podemos estar seguros de ella.",
+        "El sorteo ha terminado y existe una lista oficial en PDF.",
+        "El sorteo ha terminado y la lista de números y premios está basada en la oficial. \nDe todas formas, recuerda que la única lista oficial es la que publica la ONLAE y deberías comprobar todos tus números contra ella."
+    ]
+    response = requests.get(f'{api_url}?s=1')
+    json_data = json.loads(response.text[response.text.find('{'):])
+    if not json_data['error']:
+        print(f'{status_msg[json_data["status"]]}\n')
+
+    # Get prize for each number
     for n in lines:
         response = requests.get(f'{api_url}?n={n}')
-        json_data = json.loads(response.text[9:])
+        json_data = json.loads(response.text[response.text.find('{'):])
         result = 'error' if json_data['error'] else int(json_data['premio'])
         print(f'El número {n:05} tiene un premio de: {result}')
     
